af1ea547e7e1c8334fcdc3e757c48b64fbb2a60e
gamirkola <mirkogal@gmail.com>
2019-11-27 12:25:29 +0100

    some code

--- MitmSniffer.py
+++ MitmSniffer.py
@@ -1,13 +1,15 @@
 from scapy.all import *
-from scapy.utils import PcapWriter
 import keyboard
-from mitmproxy import *
+from mitmproxy import proxy, options
+from mitmproxy.tools.dump import DumpMaster
 from multiprocessing import *
-from packettotal_sdk import packettotal_api, search_tools
+from packettotal_sdk.packettotal_api import PacketTotalApi
 from tqdm import tqdm
-import requests
 import os
 import settings
+import json
+from pathlib import Path
+import sslkeylog
 
 
 load_layer("http")
@@ -15,23 +17,28 @@ load_layer("http")
 API_KEY = os.getenv("API_KEY")
 
 
-# funzione che probabilmente andrà a mostrare anche più dettagliatamente i pacchetti
+'''function that shaw the packet, maybe it will be modified'''
 def show_packet(packet):
     if packet:
         packet.show()
 
+'''terminal command send function'''
+def send_cmd(cmd):
+    if cmd and cmd != None:
+        p = subprocess.Popen(cmd, shell=True, executable='/bin/bash', stdout=subprocess.PIPE)
+        out, err = p.communicate()
+        return out, err
+    else:
+        return False
+
 
 class MitmSniffer:
-    
-    filter = ""
-    interface = ""
-    file_name = ""
-    pkts = []
 
     def __init__(self, filter, interface, file_name):
         self.filter = filter
         self.interface = interface
         self.file_name = file_name
+        self.pkts = []
 
     # scrittore di file pcap
     def pcap_generator(self):
@@ -43,24 +50,48 @@ class MitmSniffer:
         except:
             return False
 
-    # aggiunge un pacchetto alla lista di paccheti dello sniffer
+    '''add a packet to the program list'''
     def packet_append(self, packet):
         show_packet(packet)
         self.pkts.append(packet)
 
+    '''sniff the all the specified packet with scapy sniff function'''
     def sniffer(self):
         sn = sniff(filter=self.filter, iface=self.interface, prn=self.packet_append)
 
+    def mitm_sniffer(self):
+        # os.system("export MITMPROXY_SSLKEYLOGFILE=/home/mirko/.mitmproxy/sslnemork.log")
+        #sslkeylog.set_keylog('mitmproxykey.log')
+
+        cmd = 'export SSLKEYLOGFILE="/home/mirko/.mitmproxy/sslkeylogfile.txt"'
+        send_cmd(cmd)
+
+        ''' start mitmproxy for collecting as mitm http and https traffic with the related keys'''
+        opts = options.Options(listen_host='0.0.0.0', listen_port=8080, mode='transparent',
+                               confdir="/home/mirko/.mitmproxy/")
+        pconf = proxy.config.ProxyConfig(opts)
+        m = DumpMaster(opts)
+        m.server = proxy.server.ProxyServer(pconf)
+
+        m.run()
+
     def start_sniffing(self):
-        print("press 'Q' to quit")
+        print("press 'Q' to quit sniffing")
+
+        ''' start the sniffer implemented with scapy '''
         self.pkts = Manager().list()
         sniffing = Process(target=self.sniffer, args=self.pkts)
         sniffing.start()
+
+        mitm_sniff = Process(target=self.mitm_sniffer)
+        mitm_sniff.start()
+
         while True:
             try:
                 if keyboard.is_pressed('q'):
                     print("Grazie per avere sniffato con me ;)")
                     sniffing.terminate()
+                    mitm_sniff.terminate()
                     self.pcap_generator()
                     break
                 else:
@@ -68,13 +99,20 @@ class MitmSniffer:
             except:
                 pass
 
-
+    '''fnction that analyzes with various api the pcap file'''
     def packets_analysis(self):
-        api = search_tools.SearchTools(API_KEY)
-        analysis = api.analyze(open('./'+self.file_name, 'rb'), pcap_name='testo.pcap')
+        api = PacketTotalApi(API_KEY)
+
+        upload = api.analyze(pcap_file_obj=open('./'+self.file_name, 'rb'), pcap_name=self.file_name)
+        print(upload.status_code, upload.json())
+        id = upload.json()['id']
+        print("pcap_md5: ", id)
+        analysis = api.pcap_analysis(id)
+
         print(analysis.status_code, analysis.json())
+        with open('data.json', 'w') as outfile:
+            json.dump(analysis.json(), outfile, indent=4, sort_keys=True)
 
-        response = api.search_by_pcap(open('test.pcap', 'rb'))
 
-        print(response.status_code, response.json())
+
     
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from MitmSniffer import MitmSniffer
 
-sn = MitmSniffer("ip host 192.168.0.151", "eth0", "testo.pcap")
+sn = MitmSniffer("ip host 192.168.0.151", "eth0", "sniffing.pcap")
 sn.start_sniffing()
-sn.packets_analysis()
+#sn.packets_analysis()
